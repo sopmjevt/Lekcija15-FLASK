82f28d0021e0d3e6bdbf7f30f8ec5ebed761987d
sopmjevt <31533612+sopmjevt@users.noreply.github.com>
2025-12-03 18:31:12 +0100

    Nadaljevanje

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 import datetime
 
 #New Flask object - app
@@ -8,17 +8,40 @@ app = Flask(__name__)
 
 # Controller or HANDLER
 # Root folder
-@app.route("/")                     # poševnica pomeni kje se nahaja v tem primeru root mapa
+@app.route("/", methods=["GET"])                     # poševnica pomeni kje se nahaja v tem primeru root mapa
 def index():
     current_year = datetime.datetime.now().year
+
+    username = request.cookies.get ("username")
+    if username:
+        greating = f"Hello {"username"}!"
+    else:
+        greating = "Hello user!"    
+
     greating = "Hello, user!"
     
     return render_template("index.html", greating = greating, current_year = current_year)  #navedemop tudi vrednosti, ki jih pošiljam v index.html
 
-@app.route("/contact")
+@app.route("/contact" , methods=["GET", "POST"])
 
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        return render_template("contact.html")
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_mail = request.form.get("contact-mail")
+        contact_message = request.form.get("contact-message")       
+
+        print(contact_name)
+        print(contact_mail)
+        print(contact_message)
+
+        response = make_response(render_template("feedback.html"))
+        response.set_cookie( "username, contact_name")
+
+        return response 
+
+        # return render_template("feedback.html")    Določeno zgoraj!
 
 @app.route("/galerie")
 def galerie():
